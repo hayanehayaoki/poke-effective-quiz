301abc8bc3deb6885d528899d50107abb259be03
hayanehayaoki <hayane.hayaoki113@gmail.com>
2025-01-02 11:12:32 +0900

    fix クリアタイム計測機能を一時的に停止

--- src/Components/Page/EndPage.tsx
+++ src/Components/Page/EndPage.tsx
@@ -1,4 +1,4 @@
-import { FC, memo, useContext, useLayoutEffect, useRef, useState } from "react";
+import { FC, memo, useContext, useRef } from "react";
 import { AppContext } from "../../Constants/AppContext";
 import { useStart } from "./useStart";
 
@@ -7,19 +7,12 @@ import { useStart } from "./useStart";
  */
 export const EndPage: FC = memo(function EndPage() {
 	const { correct } = useContext(AppContext);
-	const time = useClearTime();
 	const restart = useStart();
-
-	if (time.length === 0) {
-		return null;
-	}
 
 	return (
 		<>
-			<p className="Page__endPage__result">
-				10問中、{correct}問正解！（クリアタイム：{time}）
-			</p>
-			<CopyArea correct={correct} time={time} />
+			<p className="Page__endPage__result">10問中、{correct}問正解！</p>
+			<CopyArea correct={correct} />
 			<button className="Page__startButton" onClick={restart}>
 				もう一度はじめる
 			</button>
@@ -29,14 +22,13 @@ export const EndPage: FC = memo(function EndPage() {
 
 type CopyAreaProps = {
 	correct: number;
-	time: string;
 };
 
 const CopyArea: FC<CopyAreaProps> = memo(function CopyArea(props: CopyAreaProps) {
-	const { correct, time } = props;
+	const { correct } = props;
 	const inputRef = useRef<HTMLTextAreaElement | null>(null);
 
-	const value: string = `ポケモン耐性クイズ ${time}で10問中、${correct}問正解！\nhttps://poke-effective-quiz.vercel.app/`;
+	const value: string = `ポケモン耐性クイズ 10問中、${correct}問正解！\nhttps://poke-effective-quiz.vercel.app/`;
 
 	return (
 		<div className="Page__endPage__copyArea">
@@ -53,34 +45,3 @@ const CopyArea: FC<CopyAreaProps> = memo(function CopyArea(props: CopyAreaProps)
 		</div>
 	);
 });
-
-const useClearTime = (): string => {
-	const [time, setTime] = useState<string>("");
-
-	useLayoutEffect(() => {
-		performance.mark("quizEnd");
-
-		performance.measure("clearTime", "quizStart", "quizEnd");
-
-		const clearTime = performance.getEntriesByName("clearTime");
-		const { duration } = clearTime[0];
-
-		console.log(duration);
-		// 秒に変換
-		const sec = Math.floor(duration / 1000);
-
-		if (sec < 60) {
-			setTime(`${sec}秒`);
-		} else {
-			const min = Math.floor(sec / 60);
-			const rest = sec % 60;
-			setTime(`${min}分${rest}秒`);
-		}
-
-		return () => {
-			setTime("");
-		};
-	}, []);
-
-	return time;
-};
